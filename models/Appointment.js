7aed9f795f98a8b6b283af162540fba173043016
kprakashroy <roykrish264@gmail.com>
2024-11-24 13:54:15 +0530

    first

--- models/Appointment.js
+++ models/Appointment.js
@@ -2,13 +2,31 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const Appointment = sequelize.define('Appointment', {
-  firstName: { type: DataTypes.STRING, allowNull: false },
-  lastName: { type: DataTypes.STRING, allowNull: false },
-  email: { type: DataTypes.STRING, allowNull: false },
-  timeSlot: { type: DataTypes.STRING, allowNull: false },
-  doctorName: { type: DataTypes.STRING, allowNull: false },
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    },
+  },
+  timeSlot: {
+    type: DataTypes.STRING, // Or DATE if you want precise timestamps
+    allowNull: false,
+  },
+  doctorName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
 }, {
-  timestamps: true,
+  timestamps: true, // Adds `createdAt` and `updatedAt` fields
 });
 
 module.exports = Appointment;
